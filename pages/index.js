44c69e7c2374e484c27b0fa8f598546acb36b0fe
Jhonnyssom Silva <124691096+jhonnyssomsilva@users.noreply.github.com>
2023-07-06 19:02:30 +0000

    commit initial

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <h1>
+      é amor programar não é fácil porém o simples fato dessa frase aparecer
+      para você através desse link, mostra o poder da programação, protocolos e
+      claro a internet. O fato desse link ter aberto em seu browser, é um íncio
+      de tudo. Te amo.
+    </h1>
+  );
+}
+export default Home;
