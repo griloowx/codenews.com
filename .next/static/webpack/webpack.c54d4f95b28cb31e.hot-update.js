f972dd4691c49306ea3a7a7e39219c09be0c8ec4
Jhonnyssom Felipe <jhonnyssomsilva>
2023-07-13 11:41:16 -0300

    adiciona arquivo

--- .next/static/webpack/webpack.c54d4f95b28cb31e.hot-update.js
+++ .next/static/webpack/webpack.c54d4f95b28cb31e.hot-update.js
@@ -7,12 +7,19 @@
  * or disable the default devtool with "devtool: false".
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
-self["webpackHotUpdate_N_E"]("webpack",{},
-/******/ function(__webpack_require__) { // webpackRuntimeModules
-/******/ /* webpack/runtime/getFullHash */
-/******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "7e6716fe6274a21f"; }
-/******/ }();
-/******/ 
-/******/ }
+self["webpackHotUpdate_N_E"](
+  "webpack",
+  {},
+  /******/ function (__webpack_require__) {
+    // webpackRuntimeModules
+    /******/ /* webpack/runtime/getFullHash */
+    /******/ !(function () {
+      /******/ __webpack_require__.h = function () {
+        return "7e6716fe6274a21f";
+      };
+      /******/
+    })();
+    /******/
+    /******/
+  },
 );
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-Aulas do curso.dev. 
+Aulas do curso.dev.
 
 Clone do projeto OpenSource TabNews https://www.tabnews.com.br/.
 
-No curso irei aprender do zero criar algo realmente real e funcional. Mãos a obra. 
+No curso irei aprender do zero criar algo realmente real e funcional. Mãos a obra.
