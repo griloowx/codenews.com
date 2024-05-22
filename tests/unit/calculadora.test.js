15f85c250784fcb827933cd22e9ca008f476dcd3
Jhonnyssom <jhonnyssomux@gmail.com>
2024-05-22 16:39:34 -0300

    Update aulas.txt with new course link and update next-swc-loader.js

--- tests/unit/calculadora.test.js
+++ tests/unit/calculadora.test.js
@@ -1,6 +1,11 @@
 const calculadora = require("../../models/calculadora");
 
-test("somar 2 + 2 deveria retornar 4", () => {
+test("Somar 2 + 2 deve retornar 4", () => {
   const resultado = calculadora.somar(2, 2);
   expect(resultado).toBe(4);
+});
+
+test("Somar 5 + 100 deve retornar 105", () => {
+  const resultado = calculadora.somar(5, 100);
+  expect(resultado).toBe(105);
 });
