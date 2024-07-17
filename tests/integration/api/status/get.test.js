897e7a9db5ad708ad3edbb01aeb3672435b71b40
Jhonnyssom <jhonnyssomux@gmail.com>
2024-07-17 15:24:41 -0300

    finish `/api/v1/status` endpoint

--- tests/integration/api/status/get.test.js
+++ tests/integration/api/status/get.test.js
@@ -3,10 +3,11 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json()
-  expect(responseBody.updated_at).toBeDefined()
 
   const parseUpdatedAt = new Date(responseBody.updated_at).toISOString()
   expect(responseBody.updated_at).toEqual(parseUpdatedAt)
 
   expect(responseBody.dependecies.database.version).toEqual("16.3")
+  expect(responseBody.dependecies.database.max_connections).toEqual(100)
+  expect(responseBody.dependecies.database.opened_connections).toEqual(1)
 });
